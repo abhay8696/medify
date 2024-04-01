86cb9a4819336cdb15b7a82d0fad4a991fe63783
abhay8696 <kambleabhaygautam@gmail.com>
2024-04-01 22:37:39 +0530

    created state varaible to store menu-Position on small screens

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 //styles
 import "./Button.css";
 
-const Button = ({text, buttonClass, icon}) => {
+const Button = ({text, buttonClass, icon, clickFuntion}) => {
     return (
-        <button className={`Button ${buttonClass}`}>
+        <button className={`Button ${buttonClass}`} onClick={clickFuntion}>
             {icon ? <img src={icon} className='buttonIcon' /> : null}
             <span>{text}</span>
         </button>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,37 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 //styles
 import "./Navbar.css"
 //asstes
 import sheildIcon from "../../assets/sheild.svg";
+import menuIcon from "../../assets/hamburger.svg";
 //components
 import Button from '../Button/Button';
 
 const Navbar = props => {
     //props
-
+    //states
+    const [menuPoisition, setMenuPosition] = useState("menuRight");
     //variables
     const navLinks = ["Find Doctors", "Hospitals", "Medicines", "Surgeries", "Software for Provider", "Facilities"];
     //functions
     const displayLinks = () => {
-        return navLinks.map(item => <span className='navLink'>{item}</span>)
+        return navLinks.map(item => <span className='navLink'><span className='navLink-inner'>{item}</span></span>)
     }
     return (
 
-        <>
         <nav>
+        <div className='mainNav'>
             <div className='logo'>
                 <Button icon={sheildIcon} buttonClass="logoButton"/>
                 <span className='logoText'>medify</span>
             </div>
+            <div className='navMenuButton'><Button buttonClass='whiteButton' icon={menuIcon} /></div>
             <div className='navBody'>
                 <div className='navLinksDiv'>{displayLinks()}</div>
                 <Button text="my bookings" buttonClass="largeButton "/>
             </div>
-        </nav>
+        </div>
         <div className='navbarBottom'>
             <span className='navbarBottomPatch'></span>
         </div>
-        </>
+        </nav>
     );
 };
 
