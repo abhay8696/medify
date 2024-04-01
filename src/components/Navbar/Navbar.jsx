05b519aff0bd52bbad44bb824ed8ac9dcdf1742e
abhay8696 <kambleabhaygautam@gmail.com>
2024-04-01 23:11:25 +0530

    created navbar menu with transisition for small devices

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,13 +10,31 @@ import Button from '../Button/Button';
 const Navbar = props => {
     //props
     //states
-    const [menuPoisition, setMenuPosition] = useState("menuRight");
+    const [menuPoisition, setMenuPosition] = useState("hideMenu");
     //variables
     const navLinks = ["Find Doctors", "Hospitals", "Medicines", "Surgeries", "Software for Provider", "Facilities"];
     //functions
     const displayLinks = () => {
         return navLinks.map(item => <span className='navLink'><span className='navLink-inner'>{item}</span></span>)
     }
+    const handleMenuClick = () => {
+        if(menuPoisition === "menuLeft" || menuPoisition === "hideMenu") setMenuPosition("menuRight");
+        else setMenuPosition("menuLeft");
+    }
+    const displayMenuList = () => {
+        let arr = [], delay = 0, transitionDelay;
+        // ...navLinks, 
+        navLinks.forEach((item, index) => {
+            transitionDelay = {transitionDelay: `${delay++}00ms`} 
+            return arr.push(<span style={transitionDelay} className={`menuItem ${menuPoisition}`}>{item}</span>)
+        });
+
+        arr.push(<span style={transitionDelay} className={`menuItem ${menuPoisition} menuItem-forBookings`}><Button text="my bookings" buttonClass="largeButton "/></span>);
+
+        return arr;
+    } 
+
+
     return (
 
         <nav>
@@ -25,7 +43,7 @@ const Navbar = props => {
                 <Button icon={sheildIcon} buttonClass="logoButton"/>
                 <span className='logoText'>medify</span>
             </div>
-            <div className='navMenuButton'><Button buttonClass='whiteButton' icon={menuIcon} /></div>
+            <div className='navMenuButton'><Button clickFuntion={handleMenuClick} buttonClass='whiteButton' icon={menuIcon} /></div>
             <div className='navBody'>
                 <div className='navLinksDiv'>{displayLinks()}</div>
                 <Button text="my bookings" buttonClass="largeButton "/>
@@ -33,6 +51,9 @@ const Navbar = props => {
         </div>
         <div className='navbarBottom'>
             <span className='navbarBottomPatch'></span>
+        </div>
+        <div className='menuListWrapper'>
+            {displayMenuList()}
         </div>
         </nav>
     );
