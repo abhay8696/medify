5925e81edc5688f7f7bf33c6e963bf765e405dc1
abhay8696 <kambleabhaygautam@gmail.com>
2024-04-05 00:29:05 +0530

    created CommonSuperText.jsx component which will resused in different components

---
+++ src/components/CommonSuperText/CommonSuperText.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+//styles
+import "./CommonSuperText.css"
+
+const CommonSuperText = ({text}) => {
+    return (
+        <div className='CommonSuperText'>{text}</div>
+    );
+};
+
+export default CommonSuperText;
