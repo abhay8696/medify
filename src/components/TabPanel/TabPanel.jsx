55e9bf2bf57b089b0020dffbc71f5b339bb815d3
abhay8696 <kambleabhaygautam@gmail.com>
2024-04-08 20:46:37 +0530

    created SlotsSession component inside TabPanel

--- src/components/TabPanel/TabPanel.jsx
+++ src/components/TabPanel/TabPanel.jsx
@@ -3,8 +3,26 @@ import React from 'react';
 import "./TabPanel.css"
 import Button from '../Button/Button';
 
+const morningTimings = ["08:00", "08:30", "09:00", "09:30", "10:00"]
+const afternoonTimings = ["13:00", "13:30", "14:00", "14:30", "15:00"]
+const eveningTimings = ["18:00", "18:30", "19:00", "19:30", "20:00"]
 
-
+const SlotSession = props => {
+  //props
+  const { type, timings, suffix } = props;
+  //functions
+  const displayButtons = () => {
+    return timings.map(item => {
+      return <Button text={item} buttonClass={"smallButton transperantBlueButton"}/>
+    })
+  }
+  return (
+    <div className='slotSession'>
+      <span className='sessionType'>{type}</span>
+      <span className='timeButtonsArray'>{displayButtons()}</span>
+    </div>
+  )
+}
 
 const TabPanel = props => {
     //props
@@ -19,7 +37,11 @@ const TabPanel = props => {
         {...other}
       >
         {value === index && (
-          <Button text={"11:30 AM"} buttonClass={"smallButton transperantBlueButton"}/>
+          <>
+            <SlotSession type="Morining" timings = {morningTimings} suffix="AM"/>
+            <SlotSession type="Afternoon" timings = {afternoonTimings} suffix="PM"/>
+            <SlotSession type="Evening" timings = {eveningTimings} suffix="PM"/>
+          </>
         )}
       </div>
     );
