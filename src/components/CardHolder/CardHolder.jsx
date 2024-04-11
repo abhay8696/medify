9a9eb76d74ebd73764111b4faabd0fe2f6eeda42
abhay8696 <kambleabhaygautam@gmail.com>
2024-04-11 23:00:56 +0530

    installed swiper and used on CardHolder component with custom css

--- src/components/CardHolder/CardHolder.jsx
+++ src/components/CardHolder/CardHolder.jsx
@@ -9,6 +9,13 @@ import CommonHeader from '../CommonHeader/CommonHeader';
 //variables
 import { doctorsData } from '../../allVariables';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 const CardHolder = ({type, headlibe, classForMargin}) => {
     //functions
@@ -34,11 +41,32 @@ const CardHolder = ({type, headlibe, classForMargin}) => {
         <div className={`CardHolder ${classForMargin}`}>
             {type === "persons" ? <CommonHeader text={"Our Medical Specialist"} /> : null}
             <div className='cardHolderWrapper commonContainer'>
-                {displayCards()}
+                {/* <div class="swiper">
+                    <div class="swiper-wrapper">
+                        {displayCards()}
+                    </div>
+                </div> */}
+                <Swiper
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                    }}
+                    pagination={{
+                    clickable: true,
+                    }}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    // className="mySwiper"
+                >
+                    <SwiperSlide><OfferCard cardNo={1}/></SwiperSlide>
+                    <SwiperSlide><OfferCard cardNo={2}/></SwiperSlide>
+                    <SwiperSlide><OfferCard cardNo={1}/></SwiperSlide>
+                </Swiper>
             </div>
-            <div className='radioButtonsWrapper'>
+            {/* <div className='radioButtonsWrapper'>
                 <RadioButtons />
-            </div>
+            </div> */}
         </div>
     );
 };
